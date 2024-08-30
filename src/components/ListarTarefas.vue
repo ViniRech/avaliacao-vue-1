91454e6774d4cd4dab9f40ef5fb5d0836300c39d
ViniRech <viniciusrech2007@gmail.com>
2024-08-30 11:56:45 -0300

    first commit

---
+++ src/components/ListarTarefas.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps(["tarefas"], {
+    realizadas: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <section v-for="tarefa in tarefas" :key="tarefa.id" :class="{ 'feita': tarefa.feita }"
+        @click="$emit('click', tarefa.id)">
+        <Transition name="fade">
+            <p v-if="realizadas ? tarefa.feita : !tarefa.feita">{{ tarefa.descricao }}</p>
+        </Transition>
+    </section>
+</template>
+
+<style scoped>
+.feita {
+    text-decoration: line-through;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 2s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
